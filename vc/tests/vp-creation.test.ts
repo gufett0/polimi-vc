e3313ce340030d903b5ff4f7a4b04a01d74716e7
ciccillo99 <162345860+ciccillo99@users.noreply.github.com>
2024-10-28 11:13:04 +0100

    Update vp-creation.test.ts

--- vc/tests/vp-creation.test.ts
+++ vc/tests/vp-creation.test.ts
@@ -3,9 +3,9 @@ import { agent } from '../setup-agent'; // Import your Veramo agent
 
 describe('Verifiable Presentation Creation', () => {
   it('should create a Verifiable Presentation with filtered Student VC and Polimi VC', async () => {
-    const studentDid = 'did:ethr:sepolia:student123';
-    const email = 'student@polimi.it';
-    const dkimPubKey = 'MIIBIjAN...';
+   const studentDid = 'did:ethr:sepolia:';
+    const email = 'francesco1.carbone@mail.polimi.it';
+    const dkimPubKey = 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArrqfB98shqxeHsARHTc7LYDGgzdzhXUa1ByUw2+NZCmeKXk2fDbGCCw6sN5vS9spjhU9gvY8l5ghy840xMo8YispftRf01wf66YeoB+5wk1dERhE5H1DFWMXZ7z7G1/Hp/cXjRO5nWa4dvhFVLckGDk1bFfeelFaalHSTcuW9ZILMXi8SBs9hgou1GPkj2qoJDvqY6vR6qt0ac" "q+REyyY3DEgeIXN2y5ohHTFQerYLg5TWjtzk5MxjLanQSUrS2K50JlKGVLWbAixxFr45byHP1qVVef9vP2WMnnJNJsrETsP4sL1KD5wMftAb+Ri5WDGutC5zixeYWBIw3sg17BpwIDAQAB';
 
     // Create the Verifiable Presentation
     const vp = await createVerifiablePresentation(studentDid, email, dkimPubKey);
